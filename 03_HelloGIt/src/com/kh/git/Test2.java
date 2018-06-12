67d6b8e345bb589eda79a784a1f91d6ce23a0c31
mvpdu <mvpdu@DESKTOP-AQ57AID>
2018-06-12 13:25:20 +0900

    asd

--- 03_HelloGIt/src/com/kh/git/Test2.java
+++ 03_HelloGIt/src/com/kh/git/Test2.java
@@ -4,6 +4,10 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("안녕, 깃으로 본 세상");
+		System.out.println("안녕, 깃으로 본 세상");
+		System.out.println("안녕, 깃으로 본 세상");
+		System.out.println("안녕, 깃으로 본 세상");
+		System.out.println("안녕, 깃으로 본 세상");
 	}
 
 }
